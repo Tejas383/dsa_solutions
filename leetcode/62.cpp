d9a25cbf5f5e7cb5260e68e56278f232efa734e1
Tejasvita <ttejasvita@gmail.com>
2026-03-28 17:42:27 +0530

    62: using Tabulation (Bottom-Up DP) approach

--- leetcode/62.cpp
+++ leetcode/62.cpp
@@ -3,29 +3,28 @@ using namespace std;
 
 // Approach 1: Recursion (Top-Down) / Brute Force
 // Approach 2: Recursion + Memoization (Top-Down DP)
+// Approach 3: Tabulation (Bottom-Up DP)
 
 class Solution {
 public:
-    int reach(int m, int n, vector<vector<int>>& dp) {
-        if (dp[m][n] != -1)
-            return dp[m][n];
-        
-        if (m == 0 && n == 0)
-            return dp[m][n] = 1;
-        
-        int up, left;
-        up = left = 0;
-
-        if (m != 0)
-            up = reach(m - 1, n, dp);
-        if (n != 0)
-            left = reach(m, n - 1, dp);
-
-        return dp[m][n] = up + left;
-    }
-
     int uniquePaths(int m, int n) {
-        vector<vector<int>> dp(m + 1, vector<int> (n + 1, -1));
-        return reach(m - 1, n - 1, dp);
+        vector<vector<int>> dp(m + 1, vector<int> (n + 1, 0));
+        dp[0][0] = 1;
+
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                int up, left;
+                up = left = 0;
+
+                if (i != 0)
+                    up = dp[i - 1][j];
+                if (j != 0)
+                    left = dp[i][j - 1];
+
+                dp[i][j] += up + left;
+            }
+        }
+
+        return dp[m - 1][n - 1];
     }
 };
