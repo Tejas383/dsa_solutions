ac63b4dd0898f8c783ccb25b5364758c1ec41eea
Tejasvita <ttejasvita@gmail.com>
2026-03-28 17:48:46 +0530

    62: using Space Optimized DP (1D Tabulation) approach

--- leetcode/62.cpp
+++ leetcode/62.cpp
@@ -4,27 +4,32 @@ using namespace std;
 // Approach 1: Recursion (Top-Down) / Brute Force
 // Approach 2: Recursion + Memoization (Top-Down DP)
 // Approach 3: Tabulation (Bottom-Up DP)
+// Approach 4: Space Optimized DP (1D Tabulation)
 
 class Solution {
 public:
     int uniquePaths(int m, int n) {
-        vector<vector<int>> dp(m + 1, vector<int> (n + 1, 0));
-        dp[0][0] = 1;
+        vector<int> prev(n + 1, 1);
+        // prev[0] = 1;
+        vector<int> curr(n + 1, 0);
 
-        for (int i = 0; i < m; i++) {
+        for (int i = 1; i < m; i++) {
             for (int j = 0; j < n; j++) {
-                int up, left;
-                up = left = 0;
+                if (j == 0)
+                    curr[j] = 1;
+                else {
+                    int up, left;
+                    up = left = 0;
 
-                if (i != 0)
-                    up = dp[i - 1][j];
-                if (j != 0)
-                    left = dp[i][j - 1];
+                    up = prev[j];
+                    left = curr[j - 1];
 
-                dp[i][j] += up + left;
+                    curr[j] = up + left;
+                }
             }
+            prev = curr;
         }
 
-        return dp[m - 1][n - 1];
+        return prev[n - 1];
     }
 };
