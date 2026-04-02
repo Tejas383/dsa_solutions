6ac43f5cc06db7005af3b536a1077f6832d52772
Tejasvita <ttejasvita@gmail.com>
2026-04-02 22:42:47 +0530

    3418: using Bottom-Up Dynamic Programming (Tabulation) with State Augmentation approach

--- leetcode/3418.cpp
+++ leetcode/3418.cpp
@@ -5,61 +5,95 @@ using namespace std;
 // Approach 1: Recursive Dynamic Programming (Top-Down) — without Memoization
 // Time Complexity: O(4^(m+n))
 // Space Complexity: O(m + n)
+// TLE
 
 // Approach 2: Top-Down Dynamic Programming (Memoization) with State Augmentation
 // Time complexity: O(m * n) , since skips is a constant (2)
 // Space complexity: O(m * n) , since skips is a constant (2)
 // STATE AUGMENTATION: expanding DP state to include all variables needed to uniquely define a subproblem. 
-// Here, we include 'skips' in the state to account for the number of skips used, allowing us to handle the constraint of at most 2 skips effectively.
+// Here, we include 'skips' in the state to account for the number of skips used, allowing us to handle the constraint of 2 skips.
+// TLE due to large state space (because of recursion)
+
+// Approach 3: Bottom-Up Dynamic Programming (Tabulation) with State Augmentation
+// Time complexity: O(m * n) , since skips is a constant (2)
+// Space complexity: O(m * n) , since skips is a constant (2)
+// ACCEPTED
 
 class Solution {
 public:
-    int solve(int m, int n, vector<vector<int>>& coins, int skips, vector<vector<vector<int>>>& dp) {
-        if (dp[m][n][skips] != -1) 
-            return dp[m][n][skips];
-        
-        if (m == 0 && n == 0) {
-            if (coins[m][n] < 0 && skips > 0)
-                return dp[m][n][skips] = max(coins[m][n], 0);
-            return dp[m][n][skips] = coins[m][n];
-        }
-        
-        if (m == 0) {
+  int maximumAmount(vector<vector<int>>& coins) {
+    int m = coins.size();
+    int n = coins[0].size();
+
+    vector<vector<vector<int>>> dp(m, vector<vector<int>> (n, vector<int> (3, INT_MIN)));
+    // dp[i][j][k] = maximum coins collectible till cell (i, j)
+    // using k skips (skip allowed only for negative cells)
+
+    // i -> m
+    // j -> n
+    // k -> skips
+
+    // Base case: starting cell (0,0)
+    for (int k = 0; k < 3; k++) {
+      // i == 0 && j == 0
+
+      // If negative and skips available → option to skip (take 0 instead)
+      if (coins[0][0] < 0 && k > 0)
+        dp[0][0][k] = max(coins[0][0], 0);
+      else 
+        dp[0][0][k] = coins[0][0];
+    }
+
+    for (int i = 0; i < m; i++) {
+      for (int j = 0; j < n; j++) {
+        for (int k = 0; k < 3; k++) {
+          if (i == 0 && j == 0)
+            continue;
+
+          // Transition:
+          // 1. Take current cell → coins[i][j] + max(from top, from left)
+          // 2. Skip current cell (only if negative and k > 0)
+          //    → carry forward previous value with one less skip
+          // Final answer = dp[m-1][n-1][2]
+
+          if (i == 0) {
             // go left
-            int take = coins[m][n] + solve(m, n - 1, coins, skips, dp);
+            int take = coins[i][j] + dp[i][j - 1][k];
             int skip = INT_MIN;
-            if (coins[m][n] < 0 && skips > 0)
-                skip = solve(m, n - 1, coins, skips - 1, dp);
-            return dp[m][n][skips] = max(take, skip);
-        }
-        if (n == 0) {
+            if (coins[i][j] < 0 && k > 0)
+              skip = dp[i][j - 1][k - 1];
+            
+            dp[i][j][k] = max(take, skip);
+          } else if (j == 0) {
             // go up
-            int take = coins[m][n] + solve(m - 1, n, coins, skips, dp);
+            int take = coins[i][j] + dp[i - 1][j][k];
             int skip = INT_MIN;
-            if (coins[m][n] < 0 && skips > 0)
-                skip = solve(m - 1, n, coins, skips - 1, dp);
-            return dp[m][n][skips] = max(take, skip);
+            if (coins[i][j] < 0 && k > 0)
+                skip = dp[i - 1][j][k - 1];
+            
+            dp[i][j][k] = max(take, skip);
+          } else {
+            // i != 0 && j != 0
+            int upTake = dp[i - 1][j][k];
+            int leftTake = dp[i][j - 1][k];
+
+            int take = coins[i][j] + max(leftTake, upTake);
+            int skip = INT_MIN;
+
+            if (coins[i][j] < 0 && k > 0) {
+              int upSkip = dp[i - 1][j][k - 1];
+              int leftSkip = dp[i][j - 1][k - 1];
+
+              skip = max(leftSkip, upSkip);
+            }
+
+              dp[i][j][k] = max(take, skip);                    
+          }
         }
-
-        // m != 0 && n != 0
-        int upTake = solve(m - 1, n, coins, skips, dp); 
-        int leftTake = solve(m, n - 1, coins, skips, dp);
-
-        int take = coins[m][n] + max(leftTake, upTake);
-        int skip = INT_MIN;
-
-        if (coins[m][n] < 0 && skips > 0) {
-            int upSkip = solve(m - 1, n, coins, skips - 1, dp); 
-            int leftSkip = solve(m, n - 1, coins, skips - 1, dp);
-
-            skip = max(leftSkip, upSkip);
-        }
-
-        return dp[m][n][skips] = max(take, skip);
+      }
     }
 
-    int maximumAmount(vector<vector<int>>& coins) {
-        vector<vector<vector<int>>> dp(coins.size(), vector<vector<int>> (coins[0].size(), vector<int> (3, -1)));
-        return solve(coins.size() - 1, coins[0].size() - 1, coins, 2, dp);
-    }
+    // Final answer
+    return dp[m - 1][n - 1][2];
+  }
 };
