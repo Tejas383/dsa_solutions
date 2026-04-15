1b313087b67a292a928e5f75063f135d59c7ecb2
Tejasvita <ttejasvita@gmail.com>
2026-04-15 13:21:55 +0530

    1848: using Two-Pointer Expansion (Bidirectional Linear Search) approach

--- leetcode/1848.cpp
+++ leetcode/1848.cpp
@@ -6,15 +6,29 @@ using namespace std;
 // Time Complexity = O(n)
 // Space Complexity = O(1)
 
+// Approach 2: Two-Pointer Expansion (Bidirectional Linear Search)
+// Time Complexity = O(n)
+// Space Complexity = O(1)
+// slightly better, because of early return
+
 class Solution {
 public:
     int getMinDistance(vector<int>& nums, int target, int start) {
-        int ans = INT_MAX;
-        for (int i = 0; i < nums.size(); i++) {
-            if (nums[i] == target)
-                ans = min(ans, abs(start - i));
+        int i = start;
+        int j = start;
+        while (i >= 0 || j < nums.size()) {
+            if (i >= 0) {
+                if (nums[i] == target)
+                    return abs(i - start);
+                i--;
+            }
+            if (j < nums.size()) {
+                if (nums[j] == target)
+                    return abs(j - start);
+                j++;
+            }
         }
 
-        return ans;
+        return -1;
     }
 };
