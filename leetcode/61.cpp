3f70b53cf4ea7a7e52c73d9d431d4136f8665ab0
Tejasvita <ttejasvita@gmail.com>
2026-05-05 18:25:08 +0530

    lc61: using Two Pointer (Fast–Slow) technique

--- leetcode/61.cpp
+++ leetcode/61.cpp
@@ -3,41 +3,47 @@ using namespace std;
 
 // Definition for singly-linked list.
 struct ListNode {
-    int val;
-    ListNode *next;
-    ListNode() : val(0), next(nullptr) {}
-    ListNode(int x) : val(x), next(nullptr) {}
-    ListNode(int x, ListNode *next) : val(x), next(next) {}
+  int val;
+  ListNode* next;
+  ListNode() : val(0), next(nullptr) {}
+  ListNode(int x) : val(x), next(nullptr) {}
+  ListNode(int x, ListNode* next) : val(x), next(next) {}
 };
 
-class Solution {
-public:
-    ListNode* rotateRight(ListNode* head, int k) {
-        if (!head) 
-            return nullptr;
-        if (k == 0)
-            return head;
-        
-        ListNode* temp = head;
-        ListNode* prev;
-        int size = 0;
-        while (temp) {
-            prev = temp;
-            temp = temp->next;
-            size++;
-        }
-        prev->next = head;
-        
-        k %= size;
-        int i = 1;
-        while (i < size - k) {
-            head = head->next;
-            i++;
-        }
-        ListNode* tail = head;
-        head = head->next;
-        tail->next = nullptr;
+// Approach 1 : Circular Linked List + Break at Correct Position
+// Time complexity = O(n)
+// Space complexity = O(1)
 
-        return head;
+// Approach 2 : Two Pointer (Fast–Slow) Technique
+// Time complexity = O(n)
+// Space complexity = O(1)
+
+class Solution {
+ public:
+  ListNode* rotateRight(ListNode* head, int k) {
+    if (!head) return nullptr;
+    if (k == 0) return head;
+
+    ListNode* temp = head;
+    ListNode* prev;
+    int size = 0;
+    while (temp) {
+      prev = temp;
+      temp = temp->next;
+      size++;
     }
+    prev->next = head;
+
+    k %= size;
+    int i = 1;
+    while (i < size - k) {
+      head = head->next;
+      i++;
+    }
+    ListNode* tail = head;
+    head = head->next;
+    tail->next = nullptr;
+
+    return head;
+  }
 };
