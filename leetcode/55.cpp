3a671c1a1e7aaccb9d6df3d5090ebc27326bd193
Tejasvita <ttejasvita@gmail.com>
2026-05-25 22:39:34 +0530

    lc55: Using Greedy approach

--- leetcode/55.cpp
+++ leetcode/55.cpp
@@ -1,38 +1,22 @@
-#include <queue>
 #include <vector>
 using namespace std;
 
-// Approach Name: Breadth First Search (BFS) on Array Indices
-// Time ciomplexity = O(n^2)
-// Space complexity = O(n)
+// Approach Name: Greedy
+// Time ciomplexity = O(n)
+// Space complexity = O(1)
 
 class Solution {
  public:
   bool canJump(vector<int>& nums) {
     int n = nums.size();
+    int reach = 0;
 
-    vector<bool> vis(n, false);
-    queue<int> q;
+    for (int i = 0; i < n; i++) {
+      if (i > reach) return false;
 
-    q.push(0);
-    vis[0] = true;
-
-    while (!q.empty()) {
-      int node = q.front();
-      q.pop();
-
-      if (node == n - 1) return true;
-
-      int len = nums[node];
-      for (int i = 1; i <= len; i++) {
-        int nextIdx = node + i;
-        if (nextIdx < n && !vis[nextIdx]) {
-          q.push(nextIdx);
-          vis[nextIdx] = true;
-        }
-      }
+      reach = max(reach, nums[i] + i);
     }
 
-    return false;
+    return true;
   }
 };
