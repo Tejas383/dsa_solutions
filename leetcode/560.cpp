813e2732524ddda1c18dda178b37f5d1418bc045
Tejasvita <ttejasvita@gmail.com>
2026-06-17 18:03:06 +0530

    lc560: using Prefix Sum + Hash Map Frequency Counting approach

--- leetcode/560.cpp
+++ leetcode/560.cpp
@@ -1,20 +1,29 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+// Approach Name: Prefix Sum + Hash Map Frequency Counting
+// Time ciomplexity = O(n)
+// Space complexity = O(n)
+
 class Solution {
-public:
-    int subarraySum(vector<int>& nums, int k) {
-        unordered_map<int, int> prefixSum;
-        int cnt = 0;
-        int sum = 0;
-        for (int i = 0; i < nums.size(); i++) {
-            sum += nums[i];
-            if (sum == k) cnt++;
-            int val = sum - k;
-            if (prefixSum.count(val))
-                cnt += prefixSum[val];
-            prefixSum[sum]++;
-        }
-        return cnt;
+ public:
+  int subarraySum(vector<int>& nums, int k) {
+    unordered_map<int, int> idx;
+
+    int sum = 0;
+    int cnt = 0;
+
+    for (int i = 0; i < nums.size(); i++) {
+      sum += nums[i];
+
+      if (sum == k) cnt++;
+
+      int val = sum - k;
+      if (idx.count(val)) cnt += idx[val];
+
+      idx[sum]++;
     }
+
+    return cnt;
+  }
 };
