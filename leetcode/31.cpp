17c92714b98722646fcc7ee8fee2d769c37c01f6
Tejasvita <ttejasvita@gmail.com>
2026-06-17 12:53:18 +0530

    lc31: using Pivot and Suffix Reversal approach

--- leetcode/31.cpp
+++ leetcode/31.cpp
@@ -1,31 +1,33 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+// Approach Name: Pivot and Suffix Reversal
+// Time ciomplexity = O(n)
+// Space complexity = O(1)
+
 class Solution {
-public:
-    int findMinIndex(vector<int>& nums, int start) {
-        int min = start;
-        for (int j = start + 1; j < nums.size(); j++) {
-            if (nums[j] < nums[min] && nums[j] > nums[start - 1]) {
-                min = j;
-            }
-        }
-        return min;
+ public:
+  void nextPermutation(vector<int>& nums) {
+    int i;
+
+    // find the 1st element from the end which is greater than its next element
+    // for example: [1,5,8,4,7,6,5,3,1]
+    // the required element is 4
+    for (i = nums.size() - 2; i >= 0; i--) {
+      if (nums[i + 1] > nums[i]) break;
     }
-    void nextPermutation(vector<int>& nums) {
-        bool max = true;
-        for (int i = nums.size() - 1; i > 0; i--) {
-            int el;
-            if (nums[i] > nums[i - 1]) {
-                max = false;
-                int min = findMinIndex(nums, i);
-                swap(nums[i - 1], nums[min]);
-                sort(nums.begin() + i, nums.end());
-                break;
-            }
+
+    if (i >= 0) {
+      // swap the element with its greater element from the right
+      for (int j = nums.size() - 1; j > i; j--) {
+        if (nums[j] > nums[i]) {
+          swap(nums[i], nums[j]);
+          break;
         }
-        if (max) {
-            reverse(nums.begin(), nums.end());
-        }
+      }
     }
+
+    // arrange the last elements in ascending order
+    reverse(nums.begin() + i + 1, nums.end());
+  }
 };
