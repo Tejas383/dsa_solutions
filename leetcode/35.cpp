4d3fc5a6c6377e93bb859946e0239b5078a72863
Tejasvita <ttejasvita@gmail.com>
2026-06-18 20:32:11 +0530

    lc35: using Binary Search

--- leetcode/35.cpp
+++ leetcode/35.cpp
@@ -1,31 +1,30 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+// Approach Name: Binary Search
+// Time complexity = O(log n)
+// Space complexity = O(1)
+
 class Solution {
-public:
-    int searchInsert(vector<int>& nums, int target) {
-        if (target < nums[0])
-            return 0;
-        
-        int low, mid, high;
-        low = 0;
-        high = nums.size() - 1;
-        mid = -1;
-        while (high >= low) {
-            mid = (low + high) / 2;
-            if (target < nums[mid]) {
-                high = mid - 1;
-            } else if (target > nums[mid]) {
-                low = mid + 1;
-            } else {
-                return mid;
-            }
-        }
-        if (target > nums[mid]) {
-            return mid + 1;
-        }
-        return mid;
-        // OR
-        // return low;
+ public:
+  int searchInsert(vector<int>& nums, int target) {
+    if (target > nums[nums.size() - 1]) return nums.size();
+
+    int low = 0;
+    int high = nums.size() - 1;
+    int mid;
+
+    while (low < high) {
+      mid = (low + high) / 2;
+
+      if (nums[mid] == target) return mid;
+
+      if (nums[mid] > target)
+        high = mid;
+      else
+        low = mid + 1;
     }
+
+    return low;
+  }
 };
