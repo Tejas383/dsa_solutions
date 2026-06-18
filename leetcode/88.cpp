5c7900293a3f9c14d4c6356369e4b98517f43b9e
Tejasvita <ttejasvita@gmail.com>
2026-06-18 15:05:33 +0530

    lc88: using Three-Pointer Backward Merge approach

--- leetcode/88.cpp
+++ leetcode/88.cpp
@@ -1,23 +1,27 @@
 #include <bits/stdc++.h>
+using namespace std;
+
+// Approach Name: Three-Pointer Backward Merge
+// Time complexity = O(m + n)
+// Space complexity = O(1)
 
 class Solution {
-public:
-    void merge(vector<int>& nums1, int m, vector<int>& nums2, int n) {
-        int i = m - 1;
-        int j = n - 1;
-        int k = m + n - 1;
+ public:
+  void merge(vector<int>& nums1, int m, vector<int>& nums2, int n) {
+    int i = m-- + n-- - 1;
 
-        while (i >= 0 && j >= 0) {
-            if (nums1[i] > nums2[j])
-                nums1[k] = nums1[i--];
-            else 
-                nums1[k] = nums2[j--];
-            k--;
-        }
-
-        if (i < 0) {
-            while (j >= 0)
-                nums1[k--] = nums2[j--];
-        }
+    while (m >= 0 && n >= 0) {
+      if (nums1[m] > nums2[n]) {
+        swap(nums1[m--], nums1[i--]);
+      } else {
+        nums1[i--] = nums2[n--];
+      }
     }
+
+    if (m < 0) {
+      while (n >= 0) {
+        nums1[i--] = nums2[n--];
+      }
+    }
+  }
 };
