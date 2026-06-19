bfd533a8aea589d64abda215c72d6ced7505eb04
Tejasvita <ttejasvita@gmail.com>
2026-06-19 19:29:50 +0530

    lc153: using Binary search

--- leetcode/153.cpp
+++ leetcode/153.cpp
@@ -1,20 +1,27 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+// Approach name: Modified Binary Search
+// Time complexity: O(log n)
+// Space complexity: O(1)
+
 class Solution {
-public:
-    int findMin(vector<int>& nums) {
-        int low = 0;
-        int high = nums.size() - 1;
-        int mid;
-        while (low <= high) {
-            mid = (low + high) / 2;
-            if (nums[mid] < nums[high]) {
-                high = mid;
-            } else {
-                low = mid + 1;
-            }
-        }
-        return nums[mid];
+ public:
+  int findMin(vector<int>& nums) {
+    int low = 0;
+    int high = nums.size() - 1;
+    int mid;
+
+    while (low <= high) {
+      mid = (low + high) / 2;
+
+      if (nums[mid] < nums[high]) {
+        high = mid;
+      } else {
+        low = mid + 1;
+      }
     }
+
+    return nums[mid];
+  }
 };
