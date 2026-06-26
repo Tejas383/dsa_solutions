3402bfd10921098387575056b3afe90a25549af1
Tejasvita <ttejasvita@gmail.com>
2026-06-26 11:20:45 +0530

    gfg : Meeting Rooms II : using Line Sweep approach

--- gfg/meeting-rooms-II.cpp
+++ gfg/meeting-rooms-II.cpp
@@ -3,32 +3,33 @@ using namespace std;
 
 // Link: https://www.geeksforgeeks.org/problems/attend-all-meetings-ii/1
 
-// Approach Name: Two Pointers + Sorting
+// Approach 1: Two Pointers + Sorting
 // Time ciomplexity = O(n log n)
 // Space complexity = O(1)
+
+// Approach 1: Line Sweep
+// Time ciomplexity = O(n)
+// Space complexity = O(n)
 
 class Solution {
  public:
   int minMeetingRooms(vector<int>& start, vector<int>& end) {
     // code here
-    sort(start.begin(), start.end());
-    sort(end.begin(), end.end());
+    int n = start.size();
 
-    int i, j;
-    i = j = 0;
+    map<int, int> m;
 
-    int occupied_rooms = 0;
+    for (int i = 0; i < n; i++) {
+      m[start[i]]++;
+      m[end[i]]--;
+    }
+
+    int rooms = 0;
     int ans = 0;
 
-    while (i < start.size()) {
-      if (start[i] < end[j]) {
-        occupied_rooms++;
-        i++;
-        ans = max(ans, occupied_rooms);
-      } else {
-        occupied_rooms--;
-        j++;
-      }
+    for (auto v : m) {
+      rooms += v.second;
+      ans = max(ans, rooms);
     }
 
     return ans;
