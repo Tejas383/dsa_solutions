c5c092ea785d3eb60dd016ff744af5d3f39eefb9
Tejasvita <ttejasvita@gmail.com>
2026-07-08 10:58:19 +0530

    lc3043: using Trie data structure

--- leetcode/3043.cpp
+++ leetcode/3043.cpp
@@ -1,56 +1,38 @@
-#include <algorithm>
-#include <string>
-#include <vector>
+#include <bits/stdc++.h>
 using namespace std;
 
 // Approach Name: Trie (Prefix Tree) Based Prefix Matching
 // Time ciomplexity = O((m + n) * d)
 // Space complexity = O(n * d)
 
-class TrieNode {
- public:
-  vector<TrieNode*> children = vector<TrieNode*>(10);
-
-  TrieNode() {
-    for (int i = 0; i < 10; i++) {
-      children[i] = nullptr;
-    }
-  }
-};
-
 class Trie {
- public:
-  TrieNode* root;
+  vector<Trie*> children;
 
-  Trie() { root = new TrieNode(); }
+ public:
+  Trie() { children.resize(10); }
 
   void insert(int num) {
-    TrieNode* node = root;
+    Trie* crawl = this;
 
-    string numStr = to_string(num);
-
-    for (auto digit : numStr) {
-      int idx = digit - '0';
-
-      if (node->children[idx] == nullptr) node->children[idx] = new TrieNode;
-
-      node = node->children[idx];
+    string str = to_string(num);
+    for (auto s : str) {
+      int idx = s - '0';
+      if (crawl->children[idx] == nullptr) crawl->children[idx] = new Trie();
+      crawl = crawl->children[idx];
     }
   }
 
-  int findLongestPrefix(int num) {
-    TrieNode* node = root;
+  int search(int num) {
+    Trie* crawl = this;
 
-    string numStr = to_string(num);
-
+    string str = to_string(num);
     int len = 0;
 
-    for (auto digit : numStr) {
-      int idx = digit - '0';
-
-      if (node->children[idx]) {
+    for (auto s : str) {
+      int idx = s - '0';
+      if (crawl->children[idx] != nullptr) {
         len++;
-        node = node->children[idx];
+        crawl = crawl->children[idx];
       } else
         break;
     }
@@ -62,14 +44,12 @@ class Trie {
 class Solution {
  public:
   int longestCommonPrefix(vector<int>& arr1, vector<int>& arr2) {
-    Trie trie;
+    Trie* trie = new Trie();
+    for (auto a : arr1) trie->insert(a);
 
-    for (auto num : arr1) trie.insert(num);
+    int ans = 0;
+    for (auto a : arr2) ans = max(ans, trie->search(a));
 
-    int maxLen = 0;
-
-    for (auto num : arr2) maxLen = max(maxLen, trie.findLongestPrefix(num));
-
-    return maxLen;
+    return ans;
   }
 };
