da3ba8066f48d0f75526f2078e87f04e6dd0772a
Tejasvita <ttejasvita@gmail.com>
2026-07-13 23:01:11 +0530

    lc239: using Priority Queue

--- leetcode/239.cpp
+++ leetcode/239.cpp
@@ -1,9 +1,30 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-// Approach Name: Monotonic Decreasing Deque
-// Time complexity = O(n)
+// Approach 1 : Priority Queue (Max-Heap)
+// Time complexity = O(n log n)
 // Space complexity = O(n)
+
+class Solution {
+ public:
+  vector<int> maxSlidingWindow(vector<int>& nums, int k) {
+    priority_queue<pair<int, int>> pq;
+    vector<int> ans;
+
+    for (int i = 0; i < nums.size(); i++) {
+      int j = i - k + 1;
+      while (!pq.empty() && pq.top().second < j) pq.pop();
+      pq.push({nums[i], i});
+      if (j >= 0) ans.push_back(pq.top().first);
+    }
+
+    return ans;
+  }
+};
+
+// Approach 2 : Monotonic Decreasing Deque
+// Time complexity = O(n)
+// Space complexity = O(k)
 
 class Solution {
  public:
