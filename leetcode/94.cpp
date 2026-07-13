9233e636bfdc8bde41040420ec770398dbacac03
Tejasvita <ttejasvita@gmail.com>
2026-07-13 15:37:39 +0530

    lc94: using Recursive Inorder Traversal (DFS) approach

--- leetcode/94.cpp
+++ leetcode/94.cpp
@@ -16,34 +16,55 @@ struct TreeNode {
 // Time Complexity : O(n)
 // Space Complexity : O(1)
 
+// class Solution {
+//  public:
+//   vector<int> inorderTraversal(TreeNode* root) {
+//     vector<int> traverse;
+
+//     TreeNode* curr = root;
+//     while (curr) {
+//       if (!curr->left) {
+//         traverse.push_back(curr->val);
+//         curr = curr->right;
+//       } else {
+//         TreeNode* prev = curr->left;
+
+//         while (prev->right && (prev->right != curr)) {
+//           prev = prev->right;
+//         }
+
+//         if (!prev->right) {
+//           prev->right = curr;
+//           curr = curr->left;
+//         } else {
+//           prev->right = nullptr;
+//           traverse.push_back(curr->val);
+//           curr = curr->right;
+//         }
+//       }
+//     }
+
+//     return traverse;
+//   }
+// };
+
+// Approach 2 : Recursive Inorder Traversal (DFS)
+// Time Complexity : O(n)
+// Space Complexity = O(h)
+
 class Solution {
  public:
+  void traverse(TreeNode* root, vector<int>& inOrder) {
+    if (!root) return;
+
+    traverse(root->left, inOrder);
+    inOrder.push_back(root->val);
+    traverse(root->right, inOrder);
+  }
+
   vector<int> inorderTraversal(TreeNode* root) {
-    vector<int> traverse;
-
-    TreeNode* curr = root;
-    while (curr) {
-      if (!curr->left) {
-        traverse.push_back(curr->val);
-        curr = curr->right;
-      } else {
-        TreeNode* prev = curr->left;
-
-        while (prev->right && (prev->right != curr)) {
-          prev = prev->right;
-        }
-        
-        if (!prev->right) {
-          prev->right = curr;
-          curr = curr->left;
-        } else {
-          prev->right = nullptr;
-          traverse.push_back(curr->val);
-          curr = curr->right;
-        }
-      }
-    }
-
-    return traverse;
+    vector<int> inOrder;
+    traverse(root, inOrder);
+    return inOrder;
   }
 };
