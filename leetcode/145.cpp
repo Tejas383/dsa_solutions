4ac44063b36d114fc6e670858b257eea27bcf9dc
Tejasvita <ttejasvita@gmail.com>
2026-07-13 15:58:08 +0530

    lc145: using Recursive Traversal

--- leetcode/145.cpp
+++ leetcode/145.cpp
@@ -3,40 +3,66 @@ using namespace std;
 
 // Definition for a binary tree node.
 struct TreeNode {
-    int val;
-    TreeNode *left;
-    TreeNode *right;
-    TreeNode() : val(0), left(nullptr), right(nullptr) {}
-    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
-    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+  int val;
+  TreeNode* left;
+  TreeNode* right;
+  TreeNode() : val(0), left(nullptr), right(nullptr) {}
+  TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+  TreeNode(int x, TreeNode* left, TreeNode* right)
+      : val(x), left(left), right(right) {}
 };
 
-class Solution {
-public:
-    vector<int> postorderTraversal(TreeNode* root) {
-        vector<int> traverse;
-        if (!root) 
-            return traverse;
-        
-        TreeNode* node = root;
-        stack<TreeNode*> st;
-        st.push(node);
+// Approach 1 : Morris Traversal
+// Time Complexity : O(n)
+// Space Complexity : O(1)
 
-        unordered_map<TreeNode*, int> visited;
-        while (!st.empty()) {
-            node = st.top();
-            if (!visited.count(node)) {
-                visited[node]++;
-                if (node->right)
-                    st.push(node->right);
-                if (node->left)
-                    st.push(node->left);
-            } else {
-                traverse.push_back(node->val);
-                st.pop();
-            }
-        }
-        
-        return traverse;
-    }
+// class Solution {
+// public:
+//     vector<int> postorderTraversal(TreeNode* root) {
+//         vector<int> traverse;
+//         if (!root)
+//             return traverse;
+
+//         TreeNode* node = root;
+//         stack<TreeNode*> st;
+//         st.push(node);
+
+//         unordered_map<TreeNode*, int> visited;
+//         while (!st.empty()) {
+//             node = st.top();
+//             if (!visited.count(node)) {
+//                 visited[node]++;
+//                 if (node->right)
+//                     st.push(node->right);
+//                 if (node->left)
+//                     st.push(node->left);
+//             } else {
+//                 traverse.push_back(node->val);
+//                 st.pop();
+//             }
+//         }
+
+//         return traverse;
+//     }
+// };
+
+// Approach 2 : Recursive Traversal (DFS)
+// Time Complexity : O(n)
+// Space Complexity = O(h)
+
+class Solution {
+ public:
+  void traverse(TreeNode* root, vector<int>& postOrder) {
+    if (!root) return;
+
+    traverse(root->left, postOrder);
+    traverse(root->right, postOrder);
+    postOrder.push_back(root->val);
+  }
+
+  vector<int> postorderTraversal(TreeNode* root) {
+    vector<int> postOrder;
+    traverse(root, postOrder);
+    return postOrder;
+  }
 };
