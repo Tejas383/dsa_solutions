1ac92c0139c0416f8cf7ee0cbe270957002ef828
Tejasvita <ttejasvita@gmail.com>
2026-07-13 23:19:48 +0530

    lc101: using Recursive DFS

--- leetcode/101.cpp
+++ leetcode/101.cpp
@@ -3,29 +3,29 @@ using namespace std;
 
 // Definition for a binary tree node.
 struct TreeNode {
-    int val;
-    TreeNode *left;
-    TreeNode *right;
-    TreeNode() : val(0), left(nullptr), right(nullptr) {}
-    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
-    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+  int val;
+  TreeNode* left;
+  TreeNode* right;
+  TreeNode() : val(0), left(nullptr), right(nullptr) {}
+  TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+  TreeNode(int x, TreeNode* left, TreeNode* right)
+      : val(x), left(left), right(right) {}
 };
 
+// Approach : Recursive DFS
+// Time Complexity : O(n)
+// Space Complexity = O(h)
 class Solution {
-public:
-    bool check(TreeNode* node1, TreeNode* node2) {
-        if (!node1 && !node2)
-            return true;
-        if (!node1 || !node2)
-            return false;
-        
-        if (node1->val != node2->val)
-            return false;
-        
-        return check(node1->left, node2->right) && check(node1->right, node2->left);
-    }
+ public:
+  bool check(TreeNode* p, TreeNode* q) {
+    if (!p && !q) return true;
 
-    bool isSymmetric(TreeNode* root) {
-        return check(root, root);
-    }
+    if (!p || !q) return false;
+
+    if (p->val != q->val) return false;
+
+    return check(p->left, q->right) && check(p->right, q->left);
+  }
+
+  bool isSymmetric(TreeNode* root) { return check(root, root); }
 };
