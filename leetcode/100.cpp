ad38be27ea5fe307086f9d26c2136c8540d8e8d5
Tejasvita <ttejasvita@gmail.com>
2026-07-13 23:14:07 +0530

    lc100: using Recursive DFS

--- leetcode/100.cpp
+++ leetcode/100.cpp
@@ -3,27 +3,28 @@ using namespace std;
 
 // Definition for a binary tree node.
 struct TreeNode {
-    int val;
-    TreeNode *left;
-    TreeNode *right;
-    TreeNode() : val(0), left(nullptr), right(nullptr) {}
-    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
-    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+  int val;
+  TreeNode* left;
+  TreeNode* right;
+  TreeNode() : val(0), left(nullptr), right(nullptr) {}
+  TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+  TreeNode(int x, TreeNode* left, TreeNode* right)
+      : val(x), left(left), right(right) {}
 };
 
-class Solution {
-public:
-    bool isSameTree(TreeNode* p, TreeNode* q) {
-        if (!p && !q)
-            return true;
-        if (!p || !q)
-            return false;
-        if (p->val != q->val)
-            return false;
-        
-        bool sameL = isSameTree(p->left, q->left);
-        bool sameR = isSameTree(p->right, q->right);
+// Approach : Recursive DFS
+// Time Complexity : O(n)
+// Space Complexity = O(h)
 
-        return sameL && sameR;
-    }
+class Solution {
+ public:
+  bool isSameTree(TreeNode* p, TreeNode* q) {
+    if (!p && !q) return true;
+
+    if (!p || !q) return false;
+
+    if (p->val != q->val) return false;
+
+    return isSameTree(p->left, q->left) && isSameTree(p->right, q->right);
+  }
 };
