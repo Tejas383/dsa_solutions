98274993a689f9548fc9b2123323cd534acdc107
Tejasvita <ttejasvita@gmail.com>
2026-07-13 23:22:53 +0530

    lc104: using Recursive DFS

--- leetcode/104.cpp
+++ leetcode/104.cpp
@@ -3,20 +3,24 @@ using namespace std;
 
 // Definition for a binary tree node.
 struct TreeNode {
-    int val;
-    TreeNode *left;
-    TreeNode *right;
-    TreeNode() : val(0), left(nullptr), right(nullptr) {}
-    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
-    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+  int val;
+  TreeNode* left;
+  TreeNode* right;
+  TreeNode() : val(0), left(nullptr), right(nullptr) {}
+  TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+  TreeNode(int x, TreeNode* left, TreeNode* right)
+      : val(x), left(left), right(right) {}
 };
 
+// Approach : Recursive DFS
+// Time Complexity : O(n)
+// Space Complexity = O(h)
+
 class Solution {
-public:
-    int maxDepth(TreeNode* root) {
-        if (!root) 
-            return 0;
-        
-        return (1 + max(maxDepth(root->left), maxDepth(root->right)));
-    }
+ public:
+  int maxDepth(TreeNode* root) {
+    if (!root) return 0;
+
+    return (1 + max(maxDepth(root->left), maxDepth(root->right)));
+  }
 };
