a6f5ddc5408c963cac701cd0035e273b9fc7fcd0
Tejasvita <ttejasvita@gmail.com>
2026-07-14 09:16:24 +0530

    lc111: using Recursive DFS

--- leetcode/111.cpp
+++ leetcode/111.cpp
@@ -1,37 +1,34 @@
-#include <climits>
 #include <algorithm>
+#include <climits>
 using namespace std;
 
 // Definition for a binary tree node.
 struct TreeNode {
-    int val;
-    TreeNode *left;
-    TreeNode *right;
-    TreeNode() : val(0), left(nullptr), right(nullptr) {}
-    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
-    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+  int val;
+  TreeNode* left;
+  TreeNode* right;
+  TreeNode() : val(0), left(nullptr), right(nullptr) {}
+  TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+  TreeNode(int x, TreeNode* left, TreeNode* right)
+      : val(x), left(left), right(right) {}
 };
 
 // Approach Name: DFS (Recursive Depth-First Search)
-// Time Complexity: O(N)
-// Space Complexity: O(H)
+// Time Complexity: O(n)
+// Space Complexity: O(h)
 
 class Solution {
-public:
-    int minDepth(TreeNode* root) {
-        if (!root)
-            return 0;
-        
-        if (!root->left && !root->right)
-            return 1;
-        
-        int left = INT_MAX;
-        if (root->left)
-            left = 1 + minDepth(root->left);
-        int right = INT_MAX;
-        if (root->right)
-            right = 1 + minDepth(root->right);
-        
-        return min(left, right);
-    }
+ public:
+  int minDepth(TreeNode* root) {
+    if (!root) return 0;
+
+    if (!root->left && !root->right) return 1;
+
+    int left = INT_MAX;
+    int right = INT_MAX;
+    if (root->left) left = minDepth(root->left);
+    if (root->right) right = minDepth(root->right);
+
+    return min(left, right) + 1;
+  }
 };
