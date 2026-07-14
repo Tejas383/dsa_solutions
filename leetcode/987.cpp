5f749b4be6d182a35e6428a00e1604146d853c92
Tejasvita <ttejasvita@gmail.com>
2026-07-15 00:29:48 +0530

    lc987: using Breadth-First Search (BFS) with Column-Row Mapping and Multiset

--- leetcode/987.cpp
+++ leetcode/987.cpp
@@ -3,47 +3,50 @@ using namespace std;
 
 // Definition for a binary tree node.
 struct TreeNode {
-    int val;
-    TreeNode *left;
-    TreeNode *right;
-    TreeNode() : val(0), left(nullptr), right(nullptr) {}
-    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
-    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+  int val;
+  TreeNode* left;
+  TreeNode* right;
+  TreeNode() : val(0), left(nullptr), right(nullptr) {}
+  TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+  TreeNode(int x, TreeNode* left, TreeNode* right)
+      : val(x), left(left), right(right) {}
 };
 
+// Approach Name: Breadth-First Search (BFS) with Column-Row Mapping and
+// Multiset Time Complexity: O(n) Space Complexity: O(n)
+
 class Solution {
-public:
-    vector<vector<int>> verticalTraversal(TreeNode* root) {
-        // queue<pair<node, pair<vert, level>>> 
-        queue<pair<TreeNode*, pair<int, int>>> q;
-        // map<vert, map<level, node>>
-        map<int, map<int, multiset<int>>> m;
+ public:
+  vector<vector<int>> verticalTraversal(TreeNode* root) {
+    map<int, map<int, multiset<int>>> mp;
+    // mp<row, map<col, vec>>
 
-        q.push({root, {0, 0}});
-        while (!q.empty()) {
-            auto triplet = q.front();
-            q.pop();
+    queue<pair<TreeNode*, pair<int, int>>> q;
+    // q<node, {row, col}>
+    q.push({root, {0, 0}});
 
-            TreeNode* node = triplet.first;
-            int vert = triplet.second.first;
-            int level = triplet.second.second;
+    while (!q.empty()) {
+      auto curr = q.front();
+      q.pop();
 
-            m[vert][level].insert(node->val);
-            if (node->left)
-                q.push({node->left, {vert - 1, level + 1}});
-            if (node->right)
-                q.push({node->right, {vert + 1, level + 1}});
-        }
+      auto node = curr.first;
+      int col = curr.second.first;
+      int row = curr.second.second;
 
-        vector<vector<int>> ans;
-        for (auto p : m) {
-            vector<int> col;
-            for (auto q : p.second) {
-                col.insert(col.end(), q.second.begin(), q.second.end());
-            }
-            ans.push_back(col);
-        }
+      mp[col][row].insert(node->val);
 
-        return ans;
+      if (node->left) q.push({node->left, {col - 1, row + 1}});
+      if (node->right) q.push({node->right, {col + 1, row + 1}});
     }
+
+    vector<vector<int>> ans;
+    for (auto p : mp) {
+      vector<int> vec;
+      for (auto q : p.second)
+        vec.insert(vec.end(), q.second.begin(), q.second.end());
+      ans.push_back(vec);
+    }
+
+    return ans;
+  }
 };
