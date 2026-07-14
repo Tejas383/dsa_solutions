a793de22f2b8a646924d4f4b487111138faf7fdf
Tejasvita <ttejasvita@gmail.com>
2026-07-14 10:06:12 +0530

    lc108: using Resursive Divide and Conquer

--- leetcode/108.cpp
+++ leetcode/108.cpp
@@ -3,34 +3,34 @@ using namespace std;
 
 // Definition for a binary tree node.
 struct TreeNode {
-    int val;
-    TreeNode *left;
-    TreeNode *right;
-    TreeNode() : val(0), left(nullptr), right(nullptr) {}
-    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
-    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+  int val;
+  TreeNode* left;
+  TreeNode* right;
+  TreeNode() : val(0), left(nullptr), right(nullptr) {}
+  TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+  TreeNode(int x, TreeNode* left, TreeNode* right)
+      : val(x), left(left), right(right) {}
 };
 
-// Approach Name: Divide and Conquer (Recursive construction of height-balanced BST)
-// Time Complexity: O(n)
-// Space Complexity: O(log n) recursion stack (for balanced tree)
+// Approach Name: Divide and Conquer (Recursive construction of height-balanced
+// BST) Time Complexity: O(n) Space Complexity: O(log n) recursion stack (for
+// balanced tree)
 
 class Solution {
-public:
-    TreeNode* insert(vector<int>& nums, int start, int end) {
-        if (start == end)
-            return nullptr;
-        
-        int mid = (start + end) / 2;
+ public:
+  TreeNode* insert(int start, int end, vector<int>& nums) {
+    if (start == end) return nullptr;
 
-        TreeNode* root = new TreeNode(nums[mid]);
-        root->left = insert(nums, start, mid);
-        root->right = insert(nums, mid + 1, end);
+    int mid = (start + end) / 2;
+    TreeNode* root = new TreeNode(nums[mid]);
 
-        return root;
-    }
+    root->left = insert(start, mid, nums);
+    root->right = insert(mid + 1, end, nums);
 
-    TreeNode* sortedArrayToBST(vector<int>& nums) {
-        return insert(nums, 0, nums.size());
-    }
+    return root;
+  }
+
+  TreeNode* sortedArrayToBST(vector<int>& nums) {
+    return insert(0, nums.size(), nums);
+  }
 };
