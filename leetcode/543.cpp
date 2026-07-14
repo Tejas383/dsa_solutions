3878c3586bd101516292473566e1b25456c6985e
Tejasvita <ttejasvita@gmail.com>
2026-07-14 21:29:30 +0530

    lc543: using Recursive DFS approach

--- leetcode/543.cpp
+++ leetcode/543.cpp
@@ -3,36 +3,40 @@ using namespace std;
 
 // Definition for a binary tree node.
 struct TreeNode {
-    int val;
-    TreeNode *left;
-    TreeNode *right;
-    TreeNode() : val(0), left(nullptr), right(nullptr) {}
-    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
-    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+  int val;
+  TreeNode* left;
+  TreeNode* right;
+  TreeNode() : val(0), left(nullptr), right(nullptr) {}
+  TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+  TreeNode(int x, TreeNode* left, TreeNode* right)
+      : val(x), left(left), right(right) {}
 };
 
+// Approach Name: Recursive DFS
+// Time Complexity: O(n)
+// Space Complexity: O(n)
+
 class Solution {
-public:
-    int calcHeight(TreeNode* node) {
-        if (!node)
-            return 0;
-        return (1 + max(calcHeight(node->left), calcHeight(node->right)));
-    }
+  int dia = 0;
 
-    int dia = 0;
+  int height(TreeNode* root) {
+    if (!root) return 0;
 
-    int diameterOfBinaryTree(TreeNode* root) {
-        if (!root)
-            return -1;
-        
-        int leftHeight = calcHeight(root->left);
-        int rightHeight = calcHeight(root->right);
-        
-        dia = max(dia, (leftHeight + rightHeight));
+    return max(height(root->left), height(root->right)) + 1;
+  }
 
-        int left = diameterOfBinaryTree(root->left);
-        int right = diameterOfBinaryTree(root->right);
+ public:
+  int diameterOfBinaryTree(TreeNode* root) {
+    if (!root) return -1;
 
-        return dia;
-    }
+    int leftHeight = height(root->left);
+    int rightHeight = height(root->right);
+
+    dia = max(dia, leftHeight + rightHeight);
+
+    int left = diameterOfBinaryTree(root->left);
+    int right = diameterOfBinaryTree(root->right);
+
+    return dia;
+  }
 };
