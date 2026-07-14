75f8d889b88ecf1a50194d03ee7cd09cc17e187a
Tejasvita <ttejasvita@gmail.com>
2026-07-14 23:06:06 +0530

    lc513: using Level Order Traversal using Queue

---
+++ leetcode/513.cpp
@@ -0,0 +1,46 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+// Definition for a binary tree node.
+struct TreeNode {
+  int val;
+  TreeNode* left;
+  TreeNode* right;
+  TreeNode() : val(0), left(nullptr), right(nullptr) {}
+  TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+  TreeNode(int x, TreeNode* left, TreeNode* right)
+      : val(x), left(left), right(right) {}
+};
+
+// Approach Name: Level Order Traversal using Queue
+// Time Complexity: O(n)
+// Space Complexity: O(h)
+
+class Solution {
+ public:
+  int findBottomLeftValue(TreeNode* root) {
+    vector<vector<int>> levelOrder;
+
+    queue<TreeNode*> q;
+    q.push(root);
+
+    while (!q.empty()) {
+      int s = q.size();
+      vector<int> vec;
+
+      while (s--) {
+        auto curr = q.front();
+        q.pop();
+
+        vec.push_back(curr->val);
+
+        if (curr->left) q.push(curr->left);
+        if (curr->right) q.push(curr->right);
+      }
+
+      levelOrder.push_back(vec);
+    }
+
+    return levelOrder[levelOrder.size() - 1][0];
+  }
+};
