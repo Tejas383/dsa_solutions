e706948b0381e46446adb89e67d435745a70fa27
Tejasvita <ttejasvita@gmail.com>
2026-07-14 21:51:31 +0530

    lc102: using Iterative BFS (using Queue)

--- leetcode/102.cpp
+++ leetcode/102.cpp
@@ -3,37 +3,48 @@ using namespace std;
 
 // Definition for a binary tree node.
 struct TreeNode {
-    int val;
-    TreeNode *left;
-    TreeNode *right;
-    TreeNode() : val(0), left(nullptr), right(nullptr) {}
-    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
-    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+  int val;
+  TreeNode* left;
+  TreeNode* right;
+  TreeNode() : val(0), left(nullptr), right(nullptr) {}
+  TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+  TreeNode(int x, TreeNode* left, TreeNode* right)
+      : val(x), left(left), right(right) {}
 };
 
-class Solution {
-public:
-    vector<vector<int>> levelOrder(TreeNode* root) {
-        vector<vector<int>> traverse;
-        if (!root)
-            return traverse;
-        
-        queue<TreeNode*> q;
-        q.push(root);
-        while (!q.empty()) {
-            int n = q.size();
-            vector<int> level;
-            for (int i = 0; i < n; i++) {
-                if (q.front()->left) 
-                    q.push(q.front()->left);
-                if (q.front()->right)
-                    q.push(q.front()->right);
-                level.push_back(q.front()->val);
-                q.pop();
-            }
-            traverse.push_back(level);
-        }
+// Approach Name: Iterative BFS (using Queue)
+// Time Complexity: O(n)
+// Space Complexity: O(n)
 
-        return traverse;
+class Solution {
+ public:
+  vector<vector<int>> levelOrder(TreeNode* root) {
+    vector<vector<int>> ans;
+
+    if (!root) return ans;
+
+    queue<TreeNode*> q;
+    q.push(root);
+
+    while (!q.empty()) {
+      int s = q.size();
+      vector<int> vec;
+
+      while (s--) {
+        auto curr = q.front();
+        q.pop();
+
+        vec.push_back(curr->val);
+
+        if (curr->left) 
+          q.push(curr->left);
+        if (curr->right) 
+          q.push(curr->right);
+      }
+
+      ans.push_back(vec);
     }
+
+    return ans;
+  }
 };
