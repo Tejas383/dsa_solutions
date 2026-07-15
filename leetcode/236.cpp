ba79cf54ebdac7f08d7a187749d4b5fffc90f95e
Tejasvita <ttejasvita@gmail.com>
2026-07-15 15:53:45 +0530

    lc236: using Recursive DFS

--- leetcode/236.cpp
+++ leetcode/236.cpp
@@ -3,26 +3,26 @@ using namespace std;
 
 // Definition for a binary tree node.
 struct TreeNode {
-    int val;
-    TreeNode *left;
-    TreeNode *right;
-    TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+  int val;
+  TreeNode* left;
+  TreeNode* right;
+  TreeNode(int x) : val(x), left(NULL), right(NULL) {}
 };
 
-class Solution {
-public:
-    TreeNode* lowestCommonAncestor(TreeNode* root, TreeNode* p, TreeNode* q) {
-        if (!root || root == p || root == q)
-            return root;
-        
-        TreeNode* left = lowestCommonAncestor(root->left, p, q);
-        TreeNode* right = lowestCommonAncestor(root->right, p, q);
+// Approach Name: Recursive DFS (Lowest Common Ancestor using Postorder Traversal)
+// Time Complexity: O(n)
+// Space Complexity: O(h)
 
-        if (!left)
-            return right;
-        else if (!right)
-            return left;
-        else
-            return root;
-    }
+class Solution {
+ public:
+  TreeNode* lowestCommonAncestor(TreeNode* root, TreeNode* p, TreeNode* q) {
+    if (!root || root == p || root == q) return root;
+
+    TreeNode* left = lowestCommonAncestor(root->left, p, q);
+    TreeNode* right = lowestCommonAncestor(root->right, p, q);
+
+    if (!left) return right;
+    if (!right) return left;
+    return root;
+  }
 };
