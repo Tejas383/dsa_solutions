07456d77bb22685787e97de975ef19b09b932a49
Tejasvita <ttejasvita@gmail.com>
2026-07-16 12:57:06 +0530

    gfg : Undirected Graph Cycle : using BFS

--- gfg/undirected-graph-cycle.cpp
+++ gfg/undirected-graph-cycle.cpp
@@ -3,7 +3,8 @@ using namespace std;
 
 // Link:
 // https://www.geeksforgeeks.org/problems/detect-cycle-in-an-undirected-graph/1
-// Approach Name: Recursive DFS
+
+// Approach 1: Recursive DFS
 // Time Complexity: O(V + E)
 // Space Complexity: O(V)
 
@@ -36,6 +37,58 @@ class Solution {
     for (int i = 0; i < V; i++) {
       if (!vis[i]) {
         if (dfs(i, -1, adj, vis)) return true;
+      }
+    }
+
+    return false;
+  }
+};
+
+// Approach 2: BFS
+// Time Complexity: O(V + E)
+// Space Complexity: O(V)
+
+class Solution {
+  bool bfs(queue<pair<int, int>>& q, vector<vector<int>>& adj,
+           vector<bool>& vis) {
+    while (!q.empty()) {
+      pair<int, int> curr = q.front();
+      q.pop();
+
+      int node = curr.first;
+      int parent = curr.second;
+
+      for (auto nbr : adj[node]) {
+        if (!vis[nbr]) {
+          vis[nbr] = true;
+          q.push({nbr, node});
+        } else if (nbr != parent)
+          return true;
+      }
+    }
+
+    return false;
+  }
+
+ public:
+  bool isCycle(int V, vector<vector<int>>& edges) {
+    // Code here
+    vector<vector<int>> adj(V);
+
+    for (auto edge : edges) {
+      adj[edge[0]].push_back(edge[1]);
+      adj[edge[1]].push_back(edge[0]);
+    }
+
+    vector<bool> vis(V, false);
+
+    for (int i = 0; i < V; i++) {
+      if (!vis[i]) {
+        queue<pair<int, int>> q;
+        q.push({i, -1});
+        vis[i] = true;
+
+        if (bfs(q, adj, vis)) return true;
       }
     }
 
