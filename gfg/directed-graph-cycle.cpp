3f508ea0a5f7eda43e5216ce334364501ef3c7d6
Tejasvita <ttejasvita@gmail.com>
2026-07-19 16:27:28 +0530

    gfg : Directed Graph Cycle : using BFS / Topological Sort / Kahn's Algorithm

--- gfg/directed-graph-cycle.cpp
+++ gfg/directed-graph-cycle.cpp
@@ -47,3 +47,40 @@ class Solution {
     return false;
   }
 };
+
+// Approach 2: BFS / Topological Sort / Kahn's Algorithm
+// Time Complexity: O(V + E)
+// Space Complexity: O(V)
+
+class Solution {
+ public:
+  bool isCyclic(int V, vector<vector<int>>& edges) {
+    // code here
+    vector<vector<int>> adj(V);
+    vector<int> indegree(V, 0);
+    for (auto edge : edges) {
+      adj[edge[0]].push_back(edge[1]);
+      indegree[edge[1]]++;
+    }
+
+    queue<int> q;
+    for (int i = 0; i < V; i++) {
+      if (indegree[i] == 0) q.push(i);
+    }
+
+    int vis = 0;
+    while (!q.empty()) {
+      int curr = q.front();
+      q.pop();
+
+      vis++;
+
+      for (auto nbr : adj[curr]) {
+        indegree[nbr]--;
+        if (indegree[nbr] == 0) q.push(nbr);
+      }
+    }
+
+    return !(vis == V);
+  }
+};
