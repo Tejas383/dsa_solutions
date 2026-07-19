889f0aefc0f2c99837dc767870247305c69818fe
Tejasvita <ttejasvita@gmail.com>
2026-07-19 19:09:22 +0530

    lc863: All Nodes Distance K in Binary Tree : using Parent Mapping + BFS approach

--- leetcode/863.cpp
+++ leetcode/863.cpp
@@ -1,62 +1,75 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+// Approach Name: Parent Mapping + BFS
+// Time complexity = O(n)
+// Space complexity = O(h)
+// where,
+// n = number of nodes
+// h = height of the tree
+
 // Definition for a binary tree node.
 struct TreeNode {
-    int val;
-    TreeNode *left;
-    TreeNode *right;
-    TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+  int val;
+  TreeNode* left;
+  TreeNode* right;
+  TreeNode(int x) : val(x), left(NULL), right(NULL) {}
 };
 
 class Solution {
-public:
-    vector<int> distanceK(TreeNode* root, TreeNode* target, int k) {
-        queue<TreeNode*> nodes;
-        map<TreeNode*, TreeNode*> parentMap;
-        nodes.push(root);
-        while (!nodes.empty()) {
-            auto curr = nodes.front();
-            nodes.pop();
-            
-            if (curr->left) {
-                parentMap[curr->left] = curr;
-                nodes.push(curr->left);
-            }
-            if (curr->right) {
-                parentMap[curr->right] = curr;
-                nodes.push(curr->right);
-            }
-        }
+ public:
+  vector<int> distanceK(TreeNode* root, TreeNode* target, int k) {
+    unordered_map<TreeNode*, TreeNode*> parent;
+    queue<TreeNode*> q;
+    q.push(root);
 
-        nodes.push(target);
-        int dist = 0;
-        map<TreeNode*, bool> visited;
-        while (!nodes.empty() && dist < k) {
-            int n = nodes.size();
+    while (!q.empty()) {
+      TreeNode* curr = q.front();
+      q.pop();
 
-            for (int i = 0; i < n; i++) {
-                TreeNode* curr = nodes.front();
-                nodes.pop();
-
-                if (parentMap.count(curr) && !visited.count(parentMap[curr]))
-                    nodes.push(parentMap[curr]);
-                if (curr->left && !visited.count(curr->left))
-                    nodes.push(curr->left);
-                if (curr->right && !visited.count(curr->right))
-                    nodes.push(curr->right);
-                
-                visited[curr] = true;
-            }
-            dist++;
-        }
-
-        vector<int>ans;
-        while (!nodes.empty()) {
-            ans.push_back(nodes.front()->val);
-            nodes.pop();
-        }
-
-        return ans;
+      if (curr->left) {
+        parent[curr->left] = curr;
+        q.push(curr->left);
+      }
+      if (curr->right) {
+        parent[curr->right] = curr;
+        q.push(curr->right);
+      }
     }
+
+    q.push(target);
+    unordered_set<TreeNode*> vis;
+    vis.insert(target);
+    while (!q.empty() && k) {
+      int s = q.size();
+
+      while (s--) {
+        TreeNode* curr = q.front();
+        q.pop();
+
+        if (parent.count(curr) && !vis.contains(parent[curr])) {
+          q.push(parent[curr]);
+          vis.insert(parent[curr]);
+        }
+        if (curr->left && !vis.contains(curr->left)) {
+          q.push(curr->left);
+          vis.insert(curr->left);
+        }
+        if (curr->right && !vis.contains(curr->right)) {
+          q.push(curr->right);
+          vis.insert(curr->right);
+        }
+      }
+
+      k--;
+    }
+
+    vector<int> ans;
+    while (!q.empty()) {
+      ans.push_back(q.front()->val);
+      q.pop();
+    }
+
+    return ans;
+  }
 };
