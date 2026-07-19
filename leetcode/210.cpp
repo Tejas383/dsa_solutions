1f6244f9da4be2f5dc9e312f9005737c7a273c09
Tejasvita <ttejasvita@gmail.com>
2026-07-19 16:48:16 +0530

    lc210 : Course Schedule II : using Kahn's Algorithm (BFS Topological Sort)

--- leetcode/210.cpp
+++ leetcode/210.cpp
@@ -1,39 +1,40 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+// Approach Name: Kahn's Algorithm (BFS Topological Sort)
+// Time complexity = O(v + e)
+// Space complexity = O(v + e)
+
 class Solution {
-public:
-    vector<int> findOrder(int numCourses, vector<vector<int>>& prerequisites) {
-        vector<vector<int>> adj(numCourses);
-        vector<int> inDegree(numCourses, 0);
-
-        for (auto pre : prerequisites) {
-            adj[pre[1]].push_back(pre[0]);
-            inDegree[pre[0]]++;
-        }
-
-        queue<int> q;
-        for (int i = 0; i < numCourses; i++) {
-            if (inDegree[i] == 0) 
-                q.push(i);
-        }
-
-        vector<int> vec;
-        while (!q.empty()) {
-            int node = q.front();
-            q.pop();
-
-            vec.push_back(node);
-            for (auto n : adj[node]) {
-                if (inDegree[n] > 0) {
-                    inDegree[n]--;
-                    if (inDegree[n] == 0)
-                        q.push(n);
-                }
-            }
-        }
-
-        for (auto i : inDegree)
-            if (i != 0)
-                return vector<int> ();
-
-        return vec;
+ public:
+  vector<int> findOrder(int numCourses, vector<vector<int>>& prerequisites) {
+    vector<vector<int>> adj(numCourses);
+    vector<int> indegree(numCourses, 0);
+    for (const auto& req : prerequisites) {
+      // notice that the edge goes from b to a,
+      // since to complete a, b is required
+      adj[req[1]].push_back(req[0]);
+      indegree[req[0]]++;
     }
+
+    queue<int> q;
+    for (int i = 0; i < numCourses; i++) {
+      if (indegree[i] == 0) q.push(i);
+    }
+
+    vector<int> ans;
+    while (!q.empty()) {
+      int curr = q.front();
+      q.pop();
+
+      ans.push_back(curr);
+
+      for (const auto& nbr : adj[curr]) {
+        if (--indegree[nbr] == 0) q.push(nbr);
+      }
+    }
+
+    if (ans.size() == numCourses) return ans;
+    return {};
+  }
 };
