94f19a85eb9dd73ccce842365257e90b33cf31fa
Tejasvita <ttejasvita@gmail.com>
2026-07-23 10:25:19 +0530

    lc1631 : Path with Minimum Effort : using Dijkstra's Algorithm (Greedy + Min Heap on a Grid / Minimum Bottleneck Path)

--- leetcode/1631.cpp
+++ leetcode/1631.cpp
@@ -1,49 +1,51 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+// Approach name: Dijkstra's Algorithm (Greedy + Min Heap on a Grid / Minimum
+// Bottleneck Path) Time Complexity: O((V+E)logV) Space Complexity: O(n * m)
+// where,
+// v = N × M vertices
+// e ≈ 4NM edges (each cell has at most 4 neighbors)
+
 class Solution {
-public:
-    int minimumEffortPath(vector<vector<int>>& heights) {
-        vector<int> dx = {-1, 0, 1, 0};
-        vector<int> dy = {0, -1, 0, 1};
+ public:
+  int minimumEffortPath(vector<vector<int>>& heights) {
+    int n = heights.size();
+    int m = heights[0].size();
 
-        // pq = {dist, i, j}
-        priority_queue<vector<int>, vector<vector<int>>, greater<vector<int>>> pq;
-        pq.push({0, 0, 0});
+    priority_queue<vector<int>, vector<vector<int>>, greater<vector<int>>> q;
+    q.push({0, 0, 0});
+    // q = {current effort, i, j}
 
-        vector<vector<int>> dis(heights.size(), vector<int> (heights[0].size(), INT_MAX));
-        dis[0][0] = 0;
-        
-        int ans = 0;
+    int dx[4] = {-1, 1, 0, 0};
+    int dy[4] = {0, 0, -1, 1};
 
-        while (!pq.empty()) {
-            auto t = pq.top();
-            pq.pop();
+    vector<vector<int>> effort(n, vector<int>(m, INT_MAX));
+    effort[0][0] = 0;
 
-            auto d = t[0];
-            auto i = t[1];
-            auto j = t[2];
+    while (!q.empty()) {
+      auto curr = q.top();
+      q.pop();
 
-            if (i == heights.size() - 1 && j == heights[0].size() - 1)
-                return d;
+      int e = curr[0];
+      int i = curr[1];
+      int j = curr[2];
 
-            for (int k = 0; k < 4; k++) {
-                int ii = i + dx[k];
-                int jj = j + dy[k];
+      for (int k = 0; k < 4; k++) {
+        int di = i + dx[k];
+        int dj = j + dy[k];
 
-                if (
-                    ii >= 0 && jj >= 0
-                    && ii < heights.size() && jj < heights[0].size()
-                ) {
-                    if (abs(heights[ii][jj] - heights[i][j]) < dis[ii][jj]) {
-                        int effort = max(d, abs(heights[ii][jj] - heights[i][j]));
+        if (di >= 0 && di < n && dj >= 0 && dj < m) {
+          int de = max(abs(heights[i][j] - heights[di][dj]), e);
 
-                        if (effort < dis[ii][jj]) {
-                            dis[ii][jj] = effort;
-                            pq.push({effort, ii, jj});
-                        }
-                    }
-                }
-            }
+          if (de < effort[di][dj]) {
+            effort[di][dj] = de;
+            q.push({effort[di][dj], di, dj});
+          }
         }
-
-        return ans;
+      }
     }
+
+    return effort[n - 1][m - 1];
+  }
 };
