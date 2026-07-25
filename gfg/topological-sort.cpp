62f1b0fe98ea7ee613931b89e5f8ddd7b11dbcd8
Tejasvita <ttejasvita@gmail.com>
2026-07-25 10:43:14 +0530

    gfg : Topological Sort : using DFS approach

--- gfg/topological-sort.cpp
+++ gfg/topological-sort.cpp
@@ -3,7 +3,7 @@ using namespace std;
 
 // Link: https://www.geeksforgeeks.org/problems/topological-sort/1
 
-// Approach: Kahn's Algorithm (BFS Topological Sort)
+// Approach 1: Kahn's Algorithm (BFS Topological Sort)
 // Time Complexity: O(V + E)
 // Space Complexity: O(V + E)
 
@@ -34,6 +34,51 @@ class Solution {
         indegree[nbr]--;
         if (indegree[nbr] == 0) q.push(nbr);
       }
+    }
+
+    return ans;
+  }
+};
+
+// Approach 2: DFS
+// Time Complexity: O(V + E)
+// Space Complexity: O(V + E)
+
+class Solution {
+  vector<vector<int>> adj;
+  vector<bool> vis;
+  stack<int> st;
+
+  void dfs(int node) {
+    vis[node] = true;
+
+    // dfs on adjacent vertices of the current vertex
+    for (const auto& nbr : adj[node]) {
+      if (!vis[nbr]) dfs(nbr);
+    }
+
+    // after dfs, insert into the stack
+    st.push(node);
+  }
+
+ public:
+  vector<int> topoSort(int V, vector<vector<int>>& edges) {
+    // code here
+    vis.resize(V);
+    adj.resize(V);
+    for (const auto& edge : edges) {
+      adj[edge[0]].push_back(edge[1]);
+    }
+
+    // dfs on non visited vertices
+    for (int i = 0; i < V; i++) {
+      if (!vis[i]) dfs(i);
+    }
+
+    vector<int> ans;
+    while (!st.empty()) {
+      ans.push_back(st.top());
+      st.pop();
     }
 
     return ans;
