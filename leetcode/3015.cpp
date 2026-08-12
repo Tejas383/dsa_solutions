e695b07e0f40cfb5a01393adc4f017fb575a6da5
Tejasvita <ttejasvita@gmail.com>
2026-08-12 23:23:50 +0530

    LeetCode 3015: Count the Number of Houses at a Certain Distance I : Optimised using BFS from every node (All-Pairs Shortest Path using BFS)

--- leetcode/3015.cpp
+++ leetcode/3015.cpp
@@ -61,3 +61,55 @@ class Solution {
     return ans;
   }
 };
+
+// Approach: BFS from every node (All-Pairs Shortest Path using BFS)
+// Time Complexity: O(n^2)
+// Space Complexity: O(n)
+
+class Solution {
+  vector<vector<int>> adj;
+
+  void bfs(int i, vector<int>& dist) {
+    queue<int> q;
+    q.push(i);
+    dist[i] = 0;
+
+    while (!q.empty()) {
+      int curr = q.front();
+      q.pop();
+
+      for (auto nbr : adj[curr]) {
+        if (dist[nbr] == -1) {
+          dist[nbr] = dist[curr] + 1;
+          q.push(nbr);
+        }
+      }
+    }
+  }
+
+ public:
+  vector<int> countOfPairs(int n, int x, int y) {
+    adj.resize(n + 1);
+    for (int i = 1; i < n; i++) {
+      adj[i].push_back(i + 1);
+      adj[i + 1].push_back(i);
+    }
+
+    adj[x].push_back(y);
+    adj[y].push_back(x);
+
+    vector<int> ans(n + 1, 0);
+
+    for (int i = 1; i <= n; i++) {
+      vector<int> dist(n + 1, -1);
+
+      bfs(i, dist);
+
+      for (int j = 1; j <= n; j++) ans[dist[j]]++;
+    }
+
+    ans.erase(ans.begin(), ans.begin() + 1);
+
+    return ans;
+  }
+};
