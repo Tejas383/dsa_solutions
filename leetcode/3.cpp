1ec45882cea99ac1e13028abf5dcda7b7709e8ab
Tejasvita <ttejasvita@gmail.com>
2026-08-24 13:05:11 +0530

    LeetCode 3 : Longest Substring Without Repeating Characters : using Sliding Window using Hash Map approach

--- leetcode/3.cpp
+++ leetcode/3.cpp
@@ -1,9 +1,9 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-// Approach Name: Sliding Window
-// Time ciomplexity = O(n log k)
-// Space complexity = O(n)
+// Approach Name: Sliding Window using Hash Map
+// Time complexity = O(n)
+// Space complexity = O(min(n, charset))
 
 // Idea: Maintain a window containing only unique characters.
 // Expand the window when the next character is not present.
@@ -14,34 +14,18 @@ using namespace std;
 class Solution {
  public:
   int lengthOfLongestSubstring(string s) {
-    map<char, bool> pres;
-
-    int start = 0;
-    int len = 0;
     int ans = 0;
+    unordered_map<char, int> mymap;
+    int i = 0;
+    int j = 0;
 
-    for (auto c : s) {
-      if (pres.find(c) == pres.end() || pres[c] == false) {
-        pres[c] = true;
-        len++;
-      } else {
-        while (true) {
-          pres[s[start]] = false;
-          len--;
+    while (i < s.size()) {
+      if (!mymap.count(s[i]) || mymap[s[i]] == 0)
+        mymap[s[i++]]++;
+      else
+        mymap[s[j++]]--;
 
-          if (s[start] == c) {
-            start++;
-            len++;
-            break;
-          }
-
-          start++;
-        }
-
-        pres[c] = true;
-      }
-
-      ans = max(len, ans);
+      ans = max(ans, i - j);
     }
 
     return ans;
