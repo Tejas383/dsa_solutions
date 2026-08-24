c43e140a2191fb4c0cda8d3a310a8cad040753f6
Tejasvita <ttejasvita@gmail.com>
2026-08-24 13:09:58 +0530

    LeetCode 206 : Reverse Linked List : using Iterative Pointer Reversal

--- leetcode/206.cpp
+++ leetcode/206.cpp
@@ -1,26 +1,30 @@
+// Approach name : Iterative Pointer Reversal
+// Time Complexity : O(n)
+// Space Complexity : O(1)
+
 // Definition for singly-linked list.
 struct ListNode {
-    int val;
-    ListNode *next;
-    ListNode() : val(0), next(nullptr) {}
-    ListNode(int x) : val(x), next(nullptr) {}
-    ListNode(int x, ListNode *next) : val(x), next(next) {}
+  int val;
+  ListNode* next;
+  ListNode() : val(0), next(nullptr) {}
+  ListNode(int x) : val(x), next(nullptr) {}
+  ListNode(int x, ListNode* next) : val(x), next(next) {}
 };
 
 class Solution {
-public:
-    ListNode* reverseList(ListNode* head) {
-        ListNode* curr = head;
-        ListNode* prev = nullptr;
-        ListNode* next = nullptr;
+ public:
+  ListNode* reverseList(ListNode* head) {
+    ListNode* curr = head;
+    ListNode* prev = nullptr;
+    ListNode* next = nullptr;
 
-        while (curr) {
-            next = curr->next;
-            curr->next = prev;
-            prev = curr;
-            curr = next;
-        }
-        head = prev;
-        return head;
+    while (curr) {
+      next = curr->next;
+      curr->next = prev;
+      prev = curr;
+      curr = next;
     }
+    head = prev;
+    return head;
+  }
 };
