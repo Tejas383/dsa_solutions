7d20632b78a902eff0710d0a7de3bf8fb2ff4f70
Tejasvita <ttejasvita@gmail.com>
2026-08-24 13:35:31 +0530

    LeetCode 141 : Linked List Cycle : using Floyd's Cycle Detection (Tortoise and Hare)

--- leetcode/141.cpp
+++ leetcode/141.cpp
@@ -1,27 +1,30 @@
 #include <bits/stdc++.h>
+using namespace std;
 
-/**
- * Definition for singly-linked list.
- * struct ListNode {
- *     int val;
- *     ListNode *next;
- *     ListNode(int x) : val(x), next(NULL) {}
- * };
- */
+// Definition for singly-linked list.
+struct ListNode {
+  int val;
+  ListNode* next;
+  ListNode(int x) : val(x), next(NULL) {}
+};
+
+// Approach name : Floyd's Cycle Detection (Tortoise and Hare)
+// Time Complexity : O(n)
+// Space Complexity : O(1)
+
 class Solution {
-public:
-    bool hasCycle(ListNode *head) {
-        ListNode* fast = head;
-        ListNode* slow = head;
+ public:
+  bool hasCycle(ListNode* head) {
+    ListNode* slow = head;
+    ListNode* fast = head;
 
-        while (fast && fast->next) {
-            fast = fast->next->next;
-            slow = slow->next;
-            if (fast == slow) {
-                return true;
-            }
-        }
+    while (fast && fast->next) {
+      slow = slow->next;
+      fast = fast->next->next;
 
-        return false;
+      if (slow == fast) return true;
     }
+
+    return false;
+  }
 };
